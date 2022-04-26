61bec1c94377e7a9455980fb599c05c3b06ad9bb
Danilo Silva <danilo.silva@msn.com>
2022-04-25 23:59:12 -0300

    2.5 - Encapsulamento

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,12 +1,28 @@
 class Negociacao {
     constructor(data, quantidade, valor) {
         // Cada parâmetro recebido será atribuído às propriedades da classe
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
     obtemVolume() {
-        return this.quantidade * this.valor;
+        return this._quantidade * this._valor;
+    }
+
+    getData() {
+        return this._data;
+    }
+
+    getQuantidade() {
+        return this._quantidade;
+    }
+
+    getValor() {
+        return this._valor;
+    }
+
+    getVolume() {
+        return this._quantidade * this._valor;
     }
 }
